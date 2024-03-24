d8833da83dc1539b9988a2054f8779719a3f986c
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-03-24 23:20:32 +0300

    fix: fix getTime solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,10 +32,15 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return new Intl.DateTimeFormat(undefined, {
-    timeStyle: 'medium',
-    hour12: false,
-  }).format(date);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
