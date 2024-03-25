73db00db383807fe9215075717b59570355527f4
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-03-25 16:14:16 +0300

    feat: complete getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -174,8 +174,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month - 1);
+  let weekends = 0;
+  do {
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      weekends += 1;
+    }
+    date.setDate(date.getDate() + 1);
+  } while (date.getDate() !== 1);
+  return weekends;
 }
 
 /**
